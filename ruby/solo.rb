3bd7468465101a857697153ae0cc5f7f3f23ee1f
Jason Sutter <jasonsutter87@gmail.com>
2016-04-05 10:37:54 -0700

    6.4 release 0 done

---
+++ ruby/solo.rb
@@ -0,0 +1,24 @@
+#class pizza
+	#Read-only attributes
+	#read-Write attributes 
+
+	#init
+		#-name
+		#size
+		#toppings
+		#price
+
+	#set pizza size
+	#set price
+	#Set pizza toppings
+	#Set pizza toppings
+
+
+
+	#change size
+	#remove toppings
+	#change price
+
+#Random Pizza Generator
+
+#UI Pizza Generator
