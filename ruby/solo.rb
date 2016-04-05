b303bb138ef4e9cc4649f0570263244cfc5e863c
Jason Sutter <jasonsutter87@gmail.com>
2016-04-05 12:18:06 -0700

    6.2 release 0 done

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,38 +1,51 @@
 #class pizza
 class Pizza
   #Read-only attributes
-  attr_reader :name 
+  attr_reader :name, :sauce
   #read-Write attributes
     attr_accessor :price, :size, :toppings
 
   #init Method
   def initialize(name)
-      @name = name
-      @size = ""
-      @toppings = ["Cheese"]
-      @price = 0
+    @name = name
+    @size = ""
+    if @name == "cheese"
+      @toppings = ["cheese"]
+      @sauce = "marinara"
+    elsif @name == "pepperoni"
+      @toppings = ["cheese", "pepperoni"]
+      @sauce = "marinara"
+    elsif @name == "bbq"
+      @toppings = ["cheese", "red onion", "chicken", "green onion", "cilantro"]
+      @sauce = "bbq"
+    elsif @name == "chicken garlic"
+      @toppings = ["cheese", "chicken", "green onion", "red onion", "garlic", "tomatoes", "cilantro"]
+      @sauce = "alfredo"
+    elsif @name == "cajun chicken"
+      @toppings = ["cheese", "cajun chicken", "yellow bell pepper", "red bell pepper", "green onion"]
+      @sauce = "cajun alfredo"
+      @toppings = [""]
+    else
+      @toppings = ["cheese", "chicken", "yellow bell pepper", "red bell pepper", "cilantro", "green onion", "red onion", "garlic", "tomatoes", "pepperoni"]
+      @sauce = "marinara"
     end
+    @price = 0
+  end
 
     #set_pizza_size method
-    def set_pizza_size(size)
-      @size = size
-      #set price
-      if @size == "small"
-        @price = 18.00
-      elsif @size  == "medium"
-        @price = 20.00
+  def set_pizza_size(size)
+  @size = size
+  #set price
+    if @size == "small"
+      @price = 18.00
+    elsif @size  == "medium"
+      @price = 20.00
     elsif @size  == "large"
       @price = 22.00
     else
       @price = 25.00 
     end  
-    end
-  
-  #Set pizza toppings method
-  def set_toppings(toppings)
-    @toppings = toppings
   end
-
 
   #change size
   def change_pizza_size(size)
@@ -64,14 +77,125 @@ end
 
 
 
-pizza = Pizza.new("basic")
-pizza.update
-pizza.set_pizza_size("small")
-pizza.update
-pizza.set_toppings("Chicken")
-pizza.update
+#pizza = Pizza.new("basic")
+#pizza.update
+#pizza.set_pizza_size("small")
+#pizza.update
+#pizza.set_toppings("Chicken")
+#pizza.update
 
 
-#Random Pizza Generator
 
 #UI Pizza Generator
+
+p "Welcome to the Pizza Joint! How many pizza would you like"
+pizza_count = gets.chomp.to_i
+
+until pizza_count == 0 
+  p "What kind of pizza would you want (cheese, pepperoni, bbq, chicken garlic, cajun chicken, other)"
+  pizza_type = gets.chomp.downcase
+
+  if pizza_type == "cheese"
+    p "By deafult that pizza comes with #{pizza.toppings}. Would you want to remove ingredients (yes or no)"
+    change_ingredients = gets.chomp.downcase
+    if change_ingredients == "yes"
+      until change_ingredients == "no"
+          p "Okay, What would you like to remove"
+          remove = gets.chomp.downcase
+
+          if pizza.toppings.include?(remove) == true
+            pizza.remove_topping(remove)
+          end
+
+          p "Okay the pizza now only has #{pizza.toppings} as toppings. Would you want to modify it more (yes or no)"
+          change_ingredients = gets.chomp.downcase
+      end
+    end
+	
+  elsif pizza_type == "pepperoni"
+    p "By deafult that pizza comes with #{pizza.toppings}. Would you want to remove ingredients (yes or no)"
+    change_ingredients = gets.chomp.downcase
+    if change_ingredients == "yes"
+      until change_ingredients == "no"
+          p "Okay, What would you like to remove"
+          remove = gets.chomp.downcase
+
+          if pizza.toppings.include?(remove) == true
+            pizza.remove_topping(remove)
+          end
+
+          p "Okay the pizza now only has #{pizza.toppings} as toppings. Would you want to modify it more (yes or no)"
+          change_ingredients = gets.chomp.downcase
+       end
+  	end
+  
+  elsif pizza_type == "bbq"
+    p "By deafult that pizza comes with #{pizza.toppings}. Would you want to remove ingredients (yes or no)"
+    change_ingredients = gets.chomp.downcase
+    if change_ingredients == "yes"
+      until change_ingredients == "no"
+          p "Okay, What would you like to remove"
+          remove = gets.chomp.downcase
+
+          if pizza.toppings.include?(remove) == true
+            pizza.remove_topping(remove)
+          end
+
+          p "Okay the pizza now only has #{pizza.toppings} as toppings. Would you want to modify it more (yes or no)"
+          change_ingredients = gets.chomp.downcase
+      end
+  	end
+  elsif pizza_type == "chicken garlic"
+    p "By deafult that pizza comes with #pizza.toppings}. Would you want to remove ingredients (yes or no)"
+    change_ingredients = gets.chomp.downcase
+    if change_ingredients == "yes"
+      until change_ingredients == "no"
+          p "Okay, What would you like to remove"
+          remove = gets.chomp.downcase
+
+          if pizza.toppingss.include?(remove) == true
+            pizza.remove_topping(remove)
+          end
+
+          p "Okay the pizza now only has #{pizza.toppings} as toppings. Would you want to modify it more (yes or no)"
+          change_ingredients = gets.chomp.downcase
+      end
+    end
+  elsif pizza_type == "cajun chicken"
+    p "By deafult that pizza comes with #{pizza.toppings}. Would you want to remove ingredients (yes or no)"
+    change_ingredients = gets.chomp.downcase
+    if change_ingredients == "yes"
+      until change_ingredients == "no"
+          p "Okay, What would you like to remove"
+          remove = gets.chomp.downcase
+
+          if pizza.toppings.include?(remove) == true
+            pizza.remove_topping(remove)
+          end
+
+          p "Okay the pizza now only has #{pizza.toppings} as toppings. Would you want to modify it more (yes or no)"
+          change_ingredients = gets.chomp.downcase
+      end
+    end
+  else
+    p "By deafult that pizza comes with #{pizza.toppings}. Would you want to remove ingredients (yes or no)"
+    change_ingredients = gets.chomp.downcase
+    if change_ingredients == "yes"
+      until change_ingredients == "no"
+          p "Okay, What would you like to remove"
+          remove = gets.chomp.downcase
+
+          if pizza.toppings.include?(remove) == true
+            pizza.remove_topping(remove)
+          end
+
+          p "Okay the pizza now only has #{pizza.toppings} as toppings. Would you want to modify it more (yes or no)"
+          change_ingredients = gets.chomp.downcase
+      end
+
+  	end
+  end
+	
+
+  pizza_count -= 1
+end
