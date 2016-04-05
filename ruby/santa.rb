f130e6a152e0cd182008fd07d3f508f69cad8354
Jason Sutter <jasonsutter87@gmail.com>
2016-04-05 09:31:05 -0700

    6.3 Release 3 Done

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,79 +1,74 @@
 class Santa
-	def initialize(name)
-		@name = name
-		@gender = ""
-		@ethnicity = ""
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
-	end
+  attr_reader :gender, :ethnicity
+  attr_accessor :reindeer_ranking, :age
+  
+  def initialize(name)
+    @name = name
+    @gender = ""
+    @ethnicity = ""
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
 
-	def speak
-		p "Ho, ho, ho! Haaaappy holidays, said #{@name}"
+  def speak
+    p "Ho, ho, ho! Haaaappy holidays, said #{@name}"
 
-	end
+  end
 
-	def eat_milk_and_cookies
-		if 	Random.rand(5) == 0
-			p "MMMMM #{@name} just ate an Chocolatechip Cookie"
-		elsif 	Random.rand(5)== 1
-			p "MMMMM #{@name} just ate an Snickerdoodle Cookie"
-	 	elsif 	Random.rand(5) == 2
-	 		p "MMMMM #{@name} just ate an Oreo Cookie"
- 		elsif 	Random.rand(5) == 3
- 			p "MMMMM #{@name} just ate an Sugar Cookie"
- 		else
- 			p "MMMMM #{@name} just ate an Almond Cookie"
- 		end
-	end
-	
-	def set_gender(gender)
-		@gender = gender
-	end
-	def set_ethnicity(ethnicity)
-		@ethnicity = ethnicity
-	end
-	
-	def set_reindeer_ranking(string)
-		@reindeer_ranking = string
-	end
-	
-	def set_age(age)
-		@age = age
-	end
-	
-	def update
-		p "Name: #{@name}"
-		p "Gender: #{@gender}"
-		p "Ethnic Background: #{@ethnicity}"
-		p "Reindeer Ranking: #{@reindeer_ranking}"
-		p "Age: #{@age}"
-	end
+  def eat_milk_and_cookies
+    if   Random.rand(5) == 0
+      p "MMMMM #{@name} just ate an Chocolatechip Cookie"
+    elsif   Random.rand(5)== 1
+      p "MMMMM #{@name} just ate an Snickerdoodle Cookie"
+     elsif   Random.rand(5) == 2
+       p "MMMMM #{@name} just ate an Oreo Cookie"
+     elsif   Random.rand(5) == 3
+       p "MMMMM #{@name} just ate an Sugar Cookie"
+     else
+       p "MMMMM #{@name} just ate an Almond Cookie"
+     end
+  end
+  
+  def set_gender(gender)
+    @gender = gender
+  end
+  def set_ethnicity(ethnicity)
+    @ethnicity = ethnicity
+  end
+  
+  def set_reindeer_ranking(string)
+    @reindeer_ranking = string
+  end
+  
+  def set_age(age)
+    @age = age
+  end
+  
+  def update
+    p "Name: #{@name}"
+    p "Gender: #{@gender}"
+    p "Ethnic Background: #{@ethnicity}"
+    p "Reindeer Ranking: #{@reindeer_ranking}"
+    p "Age: #{@age}"
+  end
 
-	#setter method
-	def celebrate_birthday=()
-		@age = @age + 1
-	end
+  
+  def celebrate_birthday
+    @age = @age + 1
+  end
 
-	#setter method
-	def get_mad_at=(reindeer_name)
-		if @reindeer_ranking.include?(reindeer_name) == true
-			index = @reindeer_ranking.index(reindeer_name)
-			@reindeer_ranking.insert(-1, reindeer_name)
-			@reindeer_ranking.delete_at(index)
-		end
-	end
-	
-	#getter method
-	def gender
-		@gender
-	end
-	
-	#getter method
-	def ethnicity
-		@ethnicity
-	end
-	
-	
+  
+  def get_mad_at(reindeer_name)
+    if @reindeer_ranking.include?(reindeer_name) == true
+      index = @reindeer_ranking.index(reindeer_name)
+      @reindeer_ranking.insert(-1, reindeer_name)
+      @reindeer_ranking.delete_at(index)
+    end
+  end
+  
+
+  
+  
 end
 
 santa = Santa.new("Jason Santa")
@@ -85,11 +80,9 @@ santa.eat_milk_and_cookies
 
 
 p "testing out gender....#{santa.gender}"
-santa.get_mad_at=("Rudolph")
+santa.get_mad_at("Rudolph")
 santa.update
 
 
-
-
-
-
+santa.celebrate_birthday
+santa.update
