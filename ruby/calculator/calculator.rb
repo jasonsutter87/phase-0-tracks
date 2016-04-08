1d67d0eeadbf710836f48e16c5021a7b4cc7591f
Jason Sutter <jasonsutter87@gmail.com>
2016-04-08 12:45:56 -0700

    Turning Red to Green

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,6 +1,6 @@
 class Calculator
   def add(x,y)
-    x + y
+    x + y+a
   end
 
   def subtract(x,y)
@@ -10,4 +10,9 @@ class Calculator
   def multiply(x,y)
     x * y
   end
+
+  def divide(x,y)
+    x / y
+  end
+
 end
