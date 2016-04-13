a2bc98061268fb3c3db9dfc74203a8b52816acb5
Jason Sutter <jasonsutter87@gmail.com>
2016-04-13 11:24:55 -0700

    7.2 release 0 done

---
+++ js/data_structures.js
@@ -0,0 +1,21 @@
+// 3. In your new file, declare two separate arrays:
+// an array of four colors ("blue", for instance, though you're welcome to get crazy with something like "iridescent blue/green with silver sparkles")
+
+
+var color = ["green", "blue", "light-red", "blackish grey...not black, but not grey"];
+
+// 4an array of four names that you might give a horse ("Ed" is always a solid choice to start with)
+// Write additional code that adds another color to the colors array, and another horse name to the horses array.
+var names = ["Bob", "Horsey", "Secretariat", "Allison"];
+//print color array
+console.log(color);
+
+//pushing new color to array
+color.push("orange");
+
+
+//pushing new name to array
+names.push("Alexander");
+//print color name
+console.log(names);
+
